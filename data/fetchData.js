47153528c40f2b10388c003adf7df6bbde6dac48
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-25 15:34:08 -0700

    Format data for easier retrieval

--- data/fetchData.js
+++ data/fetchData.js
@@ -4,12 +4,88 @@ const fetch = require("node-fetch");
 const apiEndpoint = "https://data.sfgov.org/api/views/u2ac-gv9v/rows.csv";
 const filename = "./data/raw/parkingData.csv";
 
+const parseRow = rawRow => {
+  if (!rawRow) return;
+  const parsedRow = [];
+  let current = 0;
+  let char = rawRow.charAt(current);
+  let enclosure = false;
+  let cell = "";
+  while (char) {
+    if (char === '"') {
+      enclosure = !enclosure;
+    }
+    if (char === "," && !enclosure) {
+      parsedRow.push(cell);
+      cell = "";
+    } else {
+      cell += char;
+    }
+    char = rawRow.charAt(++current);
+  }
+  return parsedRow;
+};
+
+const processRow = rawRow => {
+  const processedRow = [];
+  // Parse string with latitude/longitude data for block
+  const coordinates = rawRow[22]
+    .substring(13, rawRow[22].length - 2)
+    .split(", ")
+    .map(pair => pair.split(" "));
+  // Find leftmost/rightmost longitudes and topmost/bottom-most latitudes
+  processedRow[0] = coordinates.reduce(
+    (min, pair) => (parseFloat(pair[0]) < min ? parseFloat(pair[0]) : min),
+    Number.POSITIVE_INFINITY
+  );
+  processedRow[1] = coordinates.reduce(
+    (min, pair) => (parseFloat(pair[1]) < min ? parseFloat(pair[1]) : min),
+    Number.POSITIVE_INFINITY
+  );
+  processedRow[2] = coordinates.reduce(
+    (max, pair) => (parseFloat(pair[0]) > max ? parseFloat(pair[0]) : max),
+    Number.NEGATIVE_INFINITY
+  );
+  processedRow[3] = coordinates.reduce(
+    (max, pair) => (parseFloat(pair[1]) > max ? parseFloat(pair[1]) : max),
+    Number.NEGATIVE_INFINITY
+  );
+  // Map remaining data fields to desired positions in row (see raw/SAMPLE_from_sfgov.csv for original fields)
+  processedRow[4] = rawRow[20];
+  processedRow[5] = rawRow[6];
+  processedRow[6] = rawRow[14];
+  processedRow[7] = rawRow[7];
+  processedRow[8] = rawRow[15];
+  processedRow[9] = rawRow[16];
+  processedRow[10] = rawRow[17];
+  processedRow[11] = rawRow[18];
+  processedRow[12] = rawRow[19];
+  processedRow[13] = rawRow[8];
+  processedRow[14] = rawRow[9];
+  processedRow[15] = rawRow[11];
+  processedRow[16] = rawRow[12];
+  processedRow[17] = rawRow[13];
+  processedRow[18] = rawRow[21];
+  processedRow[19] = rawRow[22];
+  return processedRow.join(",");
+};
+
+const processData = data => {
+  let rows = data.split("\n").slice(1);
+  // Rows require parsing since some commas do not delineate separate fields
+  // Filtering required to ensure no empty rows
+  rows = rows.map(parseRow).filter(row => row);
+  rows = rows.map(processRow);
+  return rows.join("\n");
+};
+
 const fetchData = () => {
   fetch(apiEndpoint)
     .then(res => res.text())
-    .then(text => {
+    .then(data => {
+      const result = processData(data);
       const dest = fs.createWriteStream(filename);
-      dest.write(text, () => {
+      dest.write(result, () => {
         console.log("Successfully fetched parking data from " + apiEndpoint);
       });
     })
--- data/init.sql
+++ data/init.sql
@@ -4,30 +4,26 @@ CREATE DATABASE parking_pal_db;
 \c parking_pal_db;
 
 CREATE TABLE parking_info (
-  blockside VARCHAR(20),
-  blocksweep INT,
-  cnn INT,
-  cnnrightle VARCHAR(1),
-  corridor VARCHAR(20),
-  district VARCHAR(20),
+  ll_lon DOUBLE PRECISION,
+  ll_lat DOUBLE PRECISION,
+  ur_lon DOUBLE PRECISION,
+  ur_lat DOUBLE PRECISION,
+  day VARCHAR(10),
   fromhour VARCHAR(10),
-  holidays VARCHAR(1),
-  lf_fadd INT NOT NULL,
-  lf_toadd INT NOT NULL,
-  nhood VARCHAR(20),
-  rt_fadd INT NOT NULL,
-  rt_toadd INT NOT NULL,
-  streetname VARCHAR(20) NOT NULL,
   tohour VARCHAR(10),
+  holidays VARCHAR(1),
   week1ofmon VARCHAR(1),
   week2ofmon VARCHAR(1),
   week3ofmon VARCHAR(1),
   week4ofmon VARCHAR(1),
   week5ofmon VARCHAR(1),
-  week_day VARCHAR(10),
+  lf_fadd INT NOT NULL,
+  lf_toadd INT NOT NULL,
+  rt_fadd INT NOT NULL,
+  rt_toadd INT NOT NULL,
+  streetname VARCHAR(20) NOT NULL,
   zip_code INT,
-  geo TEXT,
-  multigeo BOOLEAN
+  geo text
 );
 
 CREATE TABLE user_info (
@@ -35,10 +31,10 @@ CREATE TABLE user_info (
   username VARCHAR(20) NOT NULL,
   pass VARCHAR(50) NOT NULL,
   is_parked BOOLEAN NOT NULL DEFAULT false,
-  latitude REAL,
-  longitude REAL,
+  longitude DECIMAL,
+  latitude DOUBLE PRECISION,
   expiration TIME,
   PRIMARY KEY (id)
 );
 
-COPY parking_info FROM '/Users/benc/Desktop/MVP/data/raw/parkingData.csv' WITH CSV HEADER DELIMITER AS ',';
+COPY parking_info FROM '/Users/benc/Desktop/MVP/data/raw/parkingData.csv' WITH DELIMITER ',' CSV;
