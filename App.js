cb9b5bbaecff0d28ca433b17733db5140d5eb56e
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-25 18:03:34 -0700

    Get basic map to render, with semi-fixed center marker

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+
+const defaultPosition = {
+  latitude: 37.78825,
+  longitude: -122.4324,
+  latitudeDelta: 0.02,
+  longitudeDelta: 0.02
+};
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: defaultPosition,
+      center: {
+        latitude: defaultPosition.latitude,
+        longitude: defaultPosition.longitude
+      }
+    };
+    this.onRegionChange = this.onRegionChange.bind(this);
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      location => this.setState(location),
+      err => console.log(err)
+    );
+  }
+
+  onRegionChange(region) {
+    console.log(region);
+    this.setState({
+      center: { latitude: region.latitude, longitude: region.longitude }
+    });
+  }
+
   render() {
+    const { region, center } = this.state;
+    console.log(center);
     return (
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.container}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-      />
+        initialRegion={region}
+        onRegionChange={this.onRegionChange}
+      >
+        <Marker
+          coordinate={center}
+          title="Marker"
+          description="Description"
+          // image={require("./assets/car_icon.png")}
+        />
+      </MapView>
       // <View style={styles.container}>
       //   <Text>Open up App.js to start working on your app!</Text>
       //   <Text>Changes you make will automatically reload.</Text>
